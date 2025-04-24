513a47541505166fd4c704c38d94e9f5e5e8c8a2
alfyandoo <alfyando2001@gmail.com>
2025-04-24 12:51:30 +0700

    fix

---
+++ utils/link-rlib.ts
@@ -0,0 +1,9 @@
+import { $ } from 'bun'
+import { join } from 'path'
+
+if (process.platform === 'win32') {
+  await $`cmd /c "mklink /J  ${join(process.cwd(), 'node_modules', 'rlib')} ${join(process.cwd(), '..', 'rlib')}"`
+} else {
+  await $`rm -rf node_modules/rlib`
+  await $`ln -s ${join(process.cwd(), 'node_modules', 'rlib')} ${join(process.cwd(), '..', 'rlib')}`
+}
